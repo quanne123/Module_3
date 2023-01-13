9f10d37b6ae4163dbcdd9238e8a7b45daefa1030
TranVanAnhQuan <quandeptrai020901@gmail.com>
2023-01-13 10:58:10 +0700

    case_study

--- case_study/case_study.sql
+++ case_study/case_study.sql
@@ -126,8 +126,42 @@ insert into loai_khach (ma_loai_khach, ten_loai_khach) values (4, 'Silver');
 insert into loai_khach (ma_loai_khach, ten_loai_khach) values (5, 'Member');
 insert into kieu_thue (ma_kieu_thue,ten_kieu_thue) values (1,'year'),(2,'month'),(3,'day'),(4,'house');
 insert into loai_dich_vu(ma_loai_dich_vu,ten_loai_dich_vu) values (1,'Villa'),(2,'House'),(3,'Room'),(4,'Room');
-insert into dich_vu(ma_dich_vu,ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,dich_vu_mien_phi_di_kem,ma_kieu_thue,ma_loai_dich_vu) values (1,'Villa Beach Front',25000,1000000,10,'vip','có hồ bơi',500,4,,3,1),(2,'House Princess 01',14000,5000000,7,'vip','có thêm bếp nướng',,3,2,2),(3,'Room Twin 01',5000,1000000,2,'normal','có tivi',,,'1 xe máy,1 xe đạp',4,3),(4,'Villa No Beach Front',22000,9000000,8,'normal','có hồ bơi',300,3,'null',3,1),(5,'House Princess 02',10000,4000000,5,'normal','có thêm bếp nướng',,3,2),(6,'Room Twin 02',3000,900000,2,'normal','có tivi',,,'1 xe máy',4,3);
-
-
+INSERT INTO tableName (ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, so_nguoi_toi_da, tieu_chuan_phong, mo_ta_tien_nghi_khac, dien_tich_ho_boi, so_tang, dich_vu_mien_phi_di_kem, ma_kieu_thue, ma_loai_dich_vu) VALUES
+	(1, 'Villa Beach Front', 25000, 1000000, 10,' vip',' Có hồ bơi', 500, 4, null, 3, 1),
+	(2, 'House Princess 01', 14000, 5000000, 7,' vip', 'Có thêm bếp nướng', null, 3, null, 2, 2),
+	(3, 'Room Twin 01', 5000, 1000000, 2, 'normal', 'Có tivi', null, null, '1 Xe máy', 1 'Xe đạp', 4, 3),
+	(4, 'Villa No Beach Front', 22000, 9000000, 8,' normal', 'Có hồ bơi', 300, 3, null, 3, 1),
+	(5, 'House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', null, 2, null, 3, 2),
+	(6,' Room Twin 02, 3000', 900000, 2, 'normal', 'Có tivi', null, null, 1 'Xe máy', 4, 3);
+INSERT INTO ma_dich_vu_di_kem (ma_dich_vu_di_kem, ten_dich_vu_di_kem, gia, don_vi, trang_thai) VALUES
+	('1', 'Karaoke', '10000', 'giờ', 'tiện nghi, hiện tại'),
+	('2', 'Thuê xe máy', '10000', 'chiếc', 'hỏng 1 xe'),
+	('3', 'Thuê xe đạp', '20000', 'chiếc', 'tốt'),
+	('4', 'Buffet buổi sáng', '15000', 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+	('5', 'Buffet buổi trưa', '90000', 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+	('6', 'Buffet buổi tối', '16000', 'suất', 'đầy đủ đồ ăn, tráng miệng');
+INSERT INTO hop_dong (ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu) VALUES
+	(1, 2020-12-08, 2020-12-08, 0, 3, 1, 3),
+	(2, 2020-07-14, 2020-07-21, 200000, 7, 3, 1),
+	(3, 2021-03-15, 2021-03-17, 50000, 3, 4, 2),
+	(4, 2021-01-14, 2021-01-18, 100000, 7, 5, 5),
+	(5, 2021-07-14, 2021-07-15, 0, 7, 2, 6),
+	(6, 2021-06-01, 2021-06-03, 0, 7, 7, 6),
+	(7, 2021-09-02, 2021-09-05, 100000, 7, 4, 4),
+	(8, 2021-06-17, 2021-06-18, 150000, 3, 4, 1),
+	(9, 2020-11-19, 2020-11-19, 0, 3, 4, 3),
+	(10, 2021-04-12, 2021-04-14, 0, 10, 3, 5),
+	(11, 2021-04-25, 2021-04-25, 0, 2, 2, 1),
+	(12, 2021-05-25, 2021-05-27, 0, 7, 10, 1);
+INSERT INTO hop_dong_chi_tiet (ma_hop_dong_chi_tiet, so_luong, ma_hop_dong, ma_dich_vu_di_kem) VALUES
+	(1, 5, 2, 4),
+	(2, 8, 2, 5),
+	(3, 15, 2, 6),
+	(4, 1, 3, 1),
+	(5, 11, 3, 2),
+	(6, 1, 1, 3),
+	(7, 2, 1, 2),
+	(8, 2, 12, 2);
+    
 														
 
